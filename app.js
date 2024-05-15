533da7e8023daedeb2ff13f8d8b7a10064bb584f
Leoryu90 <thetein@gmail.com>
2024-05-15 22:02:28 +0900

    :tada: chrome clone coding - learn_2

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-alert("hi");
+let player = {
+    name: "Ryu",
+    sayhello: function (someoneName) {
+        console.log("Hi, " + someoneName + ". my name is " + this.name + ", Nice to meet you.");
+    }
+}
+
+player.sayhello("Lee")

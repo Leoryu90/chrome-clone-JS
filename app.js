5e78b46a138a7a1e6c72edf60abdca045523455a
Leoryu90 <thetein@gmail.com>
2024-05-15 22:49:37 +0900

    :tada: chrome clone coding - learn_3

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ let player = {
     name: "Ryu",
     sayhello: function (someoneName) {
         console.log("Hi, " + someoneName + ". my name is " + this.name + ", Nice to meet you.");
+    },
+    age: 0
     }
-}
 
 player.sayhello("Lee")
+
+function inputAge(a) {
+    return a
+}
+
+player.age = inputAge(123)
+console.log(player.age)

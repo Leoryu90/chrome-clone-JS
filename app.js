6db61cd5906c0767e1c6d80fca7c1ee461e6d7c4
Leoryu90 <thetein@gmail.com>
2024-05-17 00:33:34 +0900

    :tada: chrome clone coding - learn_day3-finish

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-const title = document.getElementById("test");
+const title = document.querySelector(".hello h1")
+console.log(title)
 
-title.innerText = "Hello World!";
+title.innerText = "뭐야이건"
+
+console.dir(title)
+
+function handle() {
+    title.style.color = "blue";
+}
+
+function mouseEnterh1() {
+    title.innerText = "on mouse"
+}
+function mouseleaveh1() {
+    title.innerText = "mouse is gone"
+}
+
+title.addEventListener("click", handle);
+title.addEventListener("mouseenter", mouseEnterh1);
+title.addEventListener("mouseleave", mouseleaveh1);

9cad3fe12f68cf1a750e795b457c988624277f6c
Leoryu90 <thetein@gmail.com>
2024-05-18 21:44:07 +0900

    :tada: chrome clone coding - learn_day4_classList

--- app.js
+++ app.js
@@ -1,35 +1,13 @@
-const title = document.querySelector(".hello h1")
-console.log(title)
-
-title.innerText = "뭐야이건"
-
-console.dir(title)
-
-function random(number) {
-    return Math.floor(Math.random() * (number + 1));
-}
+const h1 = document.querySelector(".hello h1")
 
 function handle() {
-    title.style.color = "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
+    const clickedClass = "clicked"
+    if(h1.classList.contains(clickedClass)) {
+        h1.classList.remove(clickedClass);
+    }
+    else {
+        h1.classList.add(clickedClass);
+    }
 }
 
-function mouseEnterh1() {
-    title.innerText = "on mouse"
-}
-function mouseleaveh1() {
-    title.innerText = "mouse is gone"
-}
-function resizeBrowser() {
-    document.body.style.backgroundColor = "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
-}
-function copySome() {
-    alert("불법 복제 ?!")
-}
-
-
-title.addEventListener("click", handle);
-title.addEventListener("mouseenter", mouseEnterh1, true);
-title.addEventListener("mouseleave", mouseleaveh1, true);
-
-window.addEventListener("resize", resizeBrowser)
-window.addEventListener("copy", copySome)
+h1.addEventListener("click", handle);

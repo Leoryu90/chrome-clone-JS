e3d523c0f76255f9967a22e920a10733de7813a5
Leoryu90 <thetein@gmail.com>
2024-05-18 18:31:35 +0900

    :tada: chrome clone coding - learn_day4_addEventListener

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ title.innerText = "뭐야이건"
 
 console.dir(title)
 
+function random(number) {
+    return Math.floor(Math.random() * (number + 1));
+}
+
 function handle() {
-    title.style.color = "blue";
+    title.style.color = "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
 }
 
 function mouseEnterh1() {
@@ -15,7 +19,17 @@ function mouseEnterh1() {
 function mouseleaveh1() {
     title.innerText = "mouse is gone"
 }
+function resizeBrowser() {
+    document.body.style.backgroundColor = "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
+}
+function copySome() {
+    alert("불법 복제 ?!")
+}
+
 
 title.addEventListener("click", handle);
 title.addEventListener("mouseenter", mouseEnterh1, true);
 title.addEventListener("mouseleave", mouseleaveh1, true);
+
+window.addEventListener("resize", resizeBrowser)
+window.addEventListener("copy", copySome)
